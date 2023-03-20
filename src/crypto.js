c53f472e331d6e7f5eb847aba570cb9d25788022
Mauricio <lcanogiraldo@gmail.com>
2023-03-20 01:55:02 -0500

    Repository updated

--- src/crypto.js
+++ src/crypto.js
@@ -1,23 +1,23 @@
-var createHash = require('react-native-expo-crypto').createHash
+var createHash = require("@maooricio/react-native-expo-crypto").createHash;
 
-function ripemd160 (buffer) {
-  return createHash('rmd160').update(buffer).digest()
+function ripemd160(buffer) {
+  return createHash("rmd160").update(buffer).digest();
 }
 
-function sha1 (buffer) {
-  return createHash('sha1').update(buffer).digest()
+function sha1(buffer) {
+  return createHash("sha1").update(buffer).digest();
 }
 
-function sha256 (buffer) {
-  return createHash('sha256').update(buffer).digest()
+function sha256(buffer) {
+  return createHash("sha256").update(buffer).digest();
 }
 
-function hash160 (buffer) {
-  return ripemd160(sha256(buffer))
+function hash160(buffer) {
+  return ripemd160(sha256(buffer));
 }
 
-function hash256 (buffer) {
-  return sha256(sha256(buffer))
+function hash256(buffer) {
+  return sha256(sha256(buffer));
 }
 
 module.exports = {
@@ -25,5 +25,5 @@ module.exports = {
   hash256: hash256,
   ripemd160: ripemd160,
   sha1: sha1,
-  sha256: sha256
-}
+  sha256: sha256,
+};
--- src/ecdsa.js
+++ src/ecdsa.js
@@ -1,157 +1,155 @@
-var createHmac = require('react-native-expo-crypto').createHmac
-var typeforce = require('typeforce')
-var types = require('./types')
+var createHmac = require("@maooricio/react-native-expo-crypto").createHmac;
+var typeforce = require("typeforce");
+var types = require("./types");
 
-var BigInteger = require('bigi')
-var ECSignature = require('./ecsignature')
+var BigInteger = require("bigi");
+var ECSignature = require("./ecsignature");
 
-var ZERO = new Buffer([0])
-var ONE = new Buffer([1])
+var ZERO = new Buffer([0]);
+var ONE = new Buffer([1]);
 
-var ecurve = require('ecurve')
-var secp256k1 = ecurve.getCurveByName('secp256k1')
+var ecurve = require("ecurve");
+var secp256k1 = ecurve.getCurveByName("secp256k1");
 
 // https://tools.ietf.org/html/rfc6979#section-3.2
-function deterministicGenerateK (hash, x, checkSig) {
-  typeforce(types.tuple(
-    types.Hash256bit,
-    types.Buffer256bit,
-    types.Function
-  ), arguments)
+function deterministicGenerateK(hash, x, checkSig) {
+  typeforce(
+    types.tuple(types.Hash256bit, types.Buffer256bit, types.Function),
+    arguments
+  );
 
-  var k = new Buffer(32)
-  var v = new Buffer(32)
+  var k = new Buffer(32);
+  var v = new Buffer(32);
 
   // Step A, ignored as hash already provided
   // Step B
-  v.fill(1)
+  v.fill(1);
 
   // Step C
-  k.fill(0)
+  k.fill(0);
 
   // Step D
-  k = createHmac('sha256', k)
+  k = createHmac("sha256", k)
     .update(v)
     .update(ZERO)
     .update(x)
     .update(hash)
-    .digest()
+    .digest();
 
   // Step E
-  v = createHmac('sha256', k).update(v).digest()
+  v = createHmac("sha256", k).update(v).digest();
 
   // Step F
-  k = createHmac('sha256', k)
+  k = createHmac("sha256", k)
     .update(v)
     .update(ONE)
     .update(x)
     .update(hash)
-    .digest()
+    .digest();
 
   // Step G
-  v = createHmac('sha256', k).update(v).digest()
+  v = createHmac("sha256", k).update(v).digest();
 
   // Step H1/H2a, ignored as tlen === qlen (256 bit)
   // Step H2b
-  v = createHmac('sha256', k).update(v).digest()
+  v = createHmac("sha256", k).update(v).digest();
 
-  var T = BigInteger.fromBuffer(v)
+  var T = BigInteger.fromBuffer(v);
 
   // Step H3, repeat until T is within the interval [1, n - 1] and is suitable for ECDSA
   while (T.signum() <= 0 || T.compareTo(secp256k1.n) >= 0 || !checkSig(T)) {
-    k = createHmac('sha256', k)
-      .update(v)
-      .update(ZERO)
-      .digest()
+    k = createHmac("sha256", k).update(v).update(ZERO).digest();
 
-    v = createHmac('sha256', k).update(v).digest()
+    v = createHmac("sha256", k).update(v).digest();
 
     // Step H1/H2a, again, ignored as tlen === qlen (256 bit)
     // Step H2b again
-    v = createHmac('sha256', k).update(v).digest()
-    T = BigInteger.fromBuffer(v)
+    v = createHmac("sha256", k).update(v).digest();
+    T = BigInteger.fromBuffer(v);
   }
 
-  return T
+  return T;
 }
 
-var N_OVER_TWO = secp256k1.n.shiftRight(1)
+var N_OVER_TWO = secp256k1.n.shiftRight(1);
 
-function sign (hash, d) {
-  typeforce(types.tuple(types.Hash256bit, types.BigInt), arguments)
+function sign(hash, d) {
+  typeforce(types.tuple(types.Hash256bit, types.BigInt), arguments);
 
-  var x = d.toBuffer(32)
-  var e = BigInteger.fromBuffer(hash)
-  var n = secp256k1.n
-  var G = secp256k1.G
+  var x = d.toBuffer(32);
+  var e = BigInteger.fromBuffer(hash);
+  var n = secp256k1.n;
+  var G = secp256k1.G;
 
-  var r, s
+  var r, s;
   deterministicGenerateK(hash, x, function (k) {
-    var Q = G.multiply(k)
+    var Q = G.multiply(k);
 
-    if (secp256k1.isInfinity(Q)) return false
+    if (secp256k1.isInfinity(Q)) return false;
 
-    r = Q.affineX.mod(n)
-    if (r.signum() === 0) return false
+    r = Q.affineX.mod(n);
+    if (r.signum() === 0) return false;
 
-    s = k.modInverse(n).multiply(e.add(d.multiply(r))).mod(n)
-    if (s.signum() === 0) return false
+    s = k
+      .modInverse(n)
+      .multiply(e.add(d.multiply(r)))
+      .mod(n);
+    if (s.signum() === 0) return false;
 
-    return true
-  })
+    return true;
+  });
 
   // enforce low S values, see bip62: 'low s values in signatures'
   if (s.compareTo(N_OVER_TWO) > 0) {
-    s = n.subtract(s)
+    s = n.subtract(s);
   }
 
-  return new ECSignature(r, s)
+  return new ECSignature(r, s);
 }
 
-function verify (hash, signature, Q) {
-  typeforce(types.tuple(
-    types.Hash256bit,
-    types.ECSignature,
-    types.ECPoint
-  ), arguments)
+function verify(hash, signature, Q) {
+  typeforce(
+    types.tuple(types.Hash256bit, types.ECSignature, types.ECPoint),
+    arguments
+  );
 
-  var n = secp256k1.n
-  var G = secp256k1.G
+  var n = secp256k1.n;
+  var G = secp256k1.G;
 
-  var r = signature.r
-  var s = signature.s
+  var r = signature.r;
+  var s = signature.s;
 
   // 1.4.1 Enforce r and s are both integers in the interval [1, n − 1]
-  if (r.signum() <= 0 || r.compareTo(n) >= 0) return false
-  if (s.signum() <= 0 || s.compareTo(n) >= 0) return false
+  if (r.signum() <= 0 || r.compareTo(n) >= 0) return false;
+  if (s.signum() <= 0 || s.compareTo(n) >= 0) return false;
 
   // 1.4.2 H = Hash(M), already done by the user
   // 1.4.3 e = H
-  var e = BigInteger.fromBuffer(hash)
+  var e = BigInteger.fromBuffer(hash);
 
   // Compute s^-1
-  var sInv = s.modInverse(n)
+  var sInv = s.modInverse(n);
 
   // 1.4.4 Compute u1 = es^−1 mod n
   //               u2 = rs^−1 mod n
-  var u1 = e.multiply(sInv).mod(n)
-  var u2 = r.multiply(sInv).mod(n)
+  var u1 = e.multiply(sInv).mod(n);
+  var u2 = r.multiply(sInv).mod(n);
 
   // 1.4.5 Compute R = (xR, yR)
   //               R = u1G + u2Q
-  var R = G.multiplyTwo(u1, Q, u2)
+  var R = G.multiplyTwo(u1, Q, u2);
 
   // 1.4.5 (cont.) Enforce R is not at infinity
-  if (secp256k1.isInfinity(R)) return false
+  if (secp256k1.isInfinity(R)) return false;
 
   // 1.4.6 Convert the field element R.x to an integer
-  var xR = R.affineX
+  var xR = R.affineX;
 
   // 1.4.7 Set v = xR mod n
-  var v = xR.mod(n)
+  var v = xR.mod(n);
 
   // 1.4.8 If v = r, output "valid", and if v != r, output "invalid"
-  return v.equals(r)
+  return v.equals(r);
 }
 
 module.exports = {
@@ -160,5 +158,5 @@ module.exports = {
   verify: verify,
 
   // TODO: remove
-  __curve: secp256k1
-}
+  __curve: secp256k1,
+};
